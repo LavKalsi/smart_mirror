9946c8039c15716c22e52ce2ab8f10f810a2e6ce
lavhere <lavkalsi6029@gmail.com>
2025-06-05 19:56:50 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// main.js (for debugging)
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    frame: false,
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  win.loadFile(path.join(__dirname, 'index.html'));
+  win.webContents.openDevTools({ mode: 'detach' });
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
+app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
